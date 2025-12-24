8bad20dad3b07a0cb89da8c6fad78e5f45f9171b
Ea3124 <leesj6717@gmail.com>
2025-12-24 09:16:44 +0000

    feat : seperate codes

---
+++ lsj/src/00_setup.py
@@ -0,0 +1,92 @@
+# # 노트북 실행에 필요한 라이브러리를 설치합니다
+
+# !pip install opencv-python-headless==4.10.0.84
+# !pip install pandas==2.2.3
+# !pip install -U scikit-learn==1.5.2
+# !pip install albumentations==1.4.18
+# !pip install matplotlib==3.9.2
+
+# python native
+import os
+import json
+import random
+import datetime
+from functools import partial
+
+# external library
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
+from sklearn.model_selection import GroupKFold
+import albumentations as A
+
+# torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models
+
+# visualization
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+import wandb
+
+load_dotenv()  # ← 이 줄이 핵심
+
+WANDB_API_KEY = os.getenv('WANDB_API_KEY')
+os.environ["WANDB_API_KEY"] = WANDB_API_KEY
+wandb.login()
+
+# 데이터 경로를 입력하세요
+
+IMAGE_ROOT = "../data/train/DCM"
+LABEL_ROOT = "../data/train/outputs_json"
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+BATCH_SIZE = 8
+# LR = 1e-4
+LR = 1e-3
+RANDOM_SEED = 21
+
+NUM_EPOCHS = 40
+VAL_EVERY = 4
+
+SAVED_DIR = "checkpoints"
+
+if not os.path.exists(SAVED_DIR):
+    os.makedirs(SAVED_DIR)
+
+# 시각화를 위한 팔레트를 설정합니다.
+PALETTE = [
+    (220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
+    (0, 60, 100), (0, 80, 100), (0, 0, 70), (0, 0, 192), (250, 170, 30),
+    (100, 170, 30), (220, 220, 0), (175, 116, 175), (250, 0, 30), (165, 42, 42),
+    (255, 77, 255), (0, 226, 252), (182, 182, 255), (0, 82, 0), (120, 166, 157),
+    (110, 76, 0), (174, 57, 255), (199, 100, 0), (72, 0, 118), (255, 179, 240),
+    (0, 125, 92), (209, 0, 151), (188, 208, 182), (0, 220, 176),
+]
+
+# 시각화 함수입니다. 클래스가 2개 이상인 픽셀을 고려하지는 않습니다.
+def label2rgb(label):
+    image_size = label.shape[1:] + (3, )
+    image = np.zeros(image_size, dtype=np.uint8)
+
+    for i, class_label in enumerate(label):
+        image[class_label == 1] = PALETTE[i]
+
+    return image

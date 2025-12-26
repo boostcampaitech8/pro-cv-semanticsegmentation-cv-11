bd0e594538eae3ed8e339a65f0facdaaa70c2691
hyeongseokgo <102367212+hyeongseokgo@users.noreply.github.com>
2025-12-26 20:53:35 +0900

    Add files via upload

---
+++ visual/visual_run.sh
@@ -0,0 +1,6 @@
+python visual.py \
+  --csv /data/ephemeral/home/kkhs/new/outputs/output_thr0_5.csv \
+  --image_root /data/ephemeral/home/dataset/test/DCM/ \
+  --ids ID040 ID041 ID042 \
+  --save_root ./vis_results \
+  --only-class Pisiform Triquetrum

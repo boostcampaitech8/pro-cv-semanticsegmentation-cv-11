cc75504a0a37ebea40d6b74dee63e10663a5dc81
cuffyluv <cuffyluv.1@gmail.com>
2025-12-29 23:07:03 +0900

    refactor: #11 - Reorganize some project directory structure and update .gitignore

---
+++ scripts/basic_runners/train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /data/ephemeral/home/jsw_pro-cv-semanticsegmentation-cv-11/train.py --config /data/ephemeral/home/jsw_pro-cv-semanticsegmentation-cv-11/configs/hrnet_w18_unetpp_simple.yaml
